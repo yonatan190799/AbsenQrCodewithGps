1e08b8d902f242ede481496cb625d915ed17bd8f
yonatan190799 <s21710466@student.unklab.ac.id>
2023-02-12 17:14:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/absenqrcodewithgps/LoginStudent.java
@@ -0,0 +1,14 @@
+package com.example.absenqrcodewithgps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_student);
+    }
+}
