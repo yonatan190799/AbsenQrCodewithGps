6aa425d3c8191920ad669ddc0f5d8baa86610567
yonatan190799 <s21710466@student.unklab.ac.id>
2023-02-12 22:24:02 +0800

    update mainactivity

---
+++ app/src/main/java/com/example/absenqrcodewithgps/LoginLecture.java
@@ -0,0 +1,14 @@
+package com.example.absenqrcodewithgps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginLecture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_lecture);
+    }
+}
---
+++ app/src/main/java/com/example/absenqrcodewithgps/StudentProfile.java
@@ -0,0 +1,14 @@
+package com.example.absenqrcodewithgps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_profile);
+    }
+}
